23f0597815cfad81ea69ed972b5d5e9b57bff5a6
Rebecca Weisinger <rmweisinger@gmail.com>
2014-02-11 20:40:21 -0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+AirbnbForPets::Application.config.secret_key_base = '112a71ddd3a7deb43d7262115acd39000e57664b2ccd66c5c6c138c89a965f56f2a3049fa49bafb889e4997c841f13e7f415d975824f8c4c0cbb4b193f67d5f3'
